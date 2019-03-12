afbe51a76fc06882341a7894646df1ea1fb56503
Susan Tan <ssusantan9@gmail.com>
2019-03-12 01:41:00 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,3 @@
-def say_hello (name = "Ruby Programmer!")
-  puts "Hello #{name}!"
+def greeting(language="Ruby", name)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
-
-
